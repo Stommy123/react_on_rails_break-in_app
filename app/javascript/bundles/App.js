0d9588cd5a357bdeef053222bcf971a377dd184a
Tommy Suwunrut <stommy49@yahoo.com>
2018-07-19 13:29:21 -0400

    css kinda working??

--- app/javascript/bundles/App.js
+++ app/javascript/bundles/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-// import './App.css';
 import Nav from './App/Components/NavBar/NavBar.js'
-import LandingPage from './App/Components/Landing/Landing';
+import LandingPage from './App/Components/Landing/Landing.js';
+import ReactDOM from 'react-dom'
 
 class App extends Component {
   render() {
     return (
-      
+
       <div className="App">
         <Nav></Nav>
         <LandingPage></LandingPage>
