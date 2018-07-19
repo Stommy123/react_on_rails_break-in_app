27b1d3bf0f8414ffe1ceb93115de48fe5f927bdc
Tommy Suwunrut <stommy49@yahoo.com>
2018-07-19 16:23:11 -0400

    models working again

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  has_many :votes, :reports
+  has_many :votes
+  has_many :reports
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
