888a6b94446edf4121660f5c7acf91e3c0e3bde9
Tommy Suwunrut <stommy49@yahoo.com>
2018-07-24 18:49:13 -0400

    map refractored

--- app/controllers/locations_controller.rb
+++
@@ -1,5 +0,0 @@
-class LocationsController < ApplicationController
-    before_action :authenticate_user!
-  def index
-  end
-end
--- app/javascript/packs/App.js
+++ app/javascript/packs/App.js
@@ -1,14 +1,13 @@
 import ReactOnRails from 'react-on-rails';
 import ReactDOM from 'react-dom'
 import App from '../bundles/App/App.js';
-import MapBox from '../bundles/App/Components/Maps/MapBox.js'
 import Report from '../bundles/App/Components/Report.js'
-import Locations from '../bundles/App/Components/Maps/Locations.js'
+import Places from '../bundles/App/Components/Maps/Places.js'
 import Contact from '../bundles/App/Components/Contact.js'
 import About from '../bundles/App/Components/About.js'
 import Scoreboard from '../bundles/App/Components/Scoreboard.js'
 
 ReactOnRails.register({
 
-    App, MapBox, Report, Locations, Contact, About, Scoreboard
+    App, Report, Places, Contact, About, Scoreboard
 });
