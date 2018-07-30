b5a54f76f9f247e8d561e9f420e5232e0475f2eb
Whitney Lubin <whitneylubin@icloud.com>
2018-07-30 16:44:41 -0400

    bulma

--- db/schema.rb
+++ db/schema.rb
@@ -18,8 +18,8 @@ ActiveRecord::Schema.define(version: 2018_07_25_042634) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -50,21 +50,9 @@ ActiveRecord::Schema.define(version: 2018_07_25_042634) do
     t.string "description"
     t.string "category"
     t.string "photo"
-    t.integer "user_id"
+    t.bigint "user_id"
     t.index ["latitude", "longitude"], name: "index_places_on_latitude_and_longitude"
     t.index ["user_id"], name: "index_places_on_user_id"
-  end
-
-  create_table "reports", force: :cascade do |t|
-    t.string "description"
-    t.float "lat"
-    t.float "lng"
-    t.string "photo"
-    t.string "category"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "user_id"
-    t.index ["user_id"], name: "index_reports_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -92,12 +80,13 @@ ActiveRecord::Schema.define(version: 2018_07_25_042634) do
     t.string "type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
-    t.integer "report_id"
-    t.integer "place_id"
+    t.bigint "user_id"
+    t.bigint "place_id"
     t.index ["place_id"], name: "index_votes_on_place_id"
-    t.index ["report_id"], name: "index_votes_on_report_id"
     t.index ["user_id"], name: "index_votes_on_user_id"
   end
 
+  add_foreign_key "places", "users"
+  add_foreign_key "votes", "places"
+  add_foreign_key "votes", "users"
 end
