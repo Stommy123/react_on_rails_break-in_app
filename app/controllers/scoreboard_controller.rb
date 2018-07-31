6f499ff1425ac2b01e8383337d34ab9d2cf37a0c
Tommy Suwunrut <stommy49@yahoo.com>
2018-07-31 11:31:24 -0400

    maybe

--- app/controllers/scoreboard_controller.rb
+++ app/controllers/scoreboard_controller.rb
@@ -1,17 +1,3 @@
-# class ScoreboardController < ApplicationController
-#   def index
-#     respond_to do |format|
-#       format.html
-#       format.json do
-#         @top_scores = User.order(points: :desc).limit(10)
-# #         @top_scores = User.where.not(:points => nil).order(points: :desc).limit(10)
-# ​        render json: @top_scores
-#       end
-#     end
-#   end
-# end
-
-
 class ScoreboardController < ApplicationController
   def index
     respond_to do |format|
