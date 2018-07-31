c95c047976fa6c3e7df0843bdf7db9c8fa5926b8
Tommy Suwunrut <stommy49@yahoo.com>
2018-07-30 22:28:35 -0400

    check branch name

--- app/javascript/bundles/App/Components/Scores/Scoreboard.js
+++ app/javascript/bundles/App/Components/Scores/Scoreboard.js
@@ -6,7 +6,6 @@ import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-// import Button from '@material-ui/core/Button';
 import Nav from '../NavBar.js';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
